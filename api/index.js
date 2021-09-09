39d8f685ad42067b594fc5741f7d824be4642293
Arik Savage <ariksavage@MacBook-Pro.local>
2021-09-08 21:39:37 -0500

    add vagrant

--- api/index.js
+++ api/index.js
@@ -8,27 +8,11 @@ const fs = require('fs');
 
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
-// Calling the readFileSync() method
-// to read 'input.txt' file
-const dbConfigData = fs.readFileSync('config/db.config',
-            {encoding:'utf8', flag:'r'});
+
+const dbConfigData = fs.readFileSync('config/db.config', {encoding:'utf8', flag:'r'});
 
 dbConfig = JSON.parse(dbConfigData);
 const db = new Database(dbConfig);
 
-api.get('/api/race/list', (req, res) => {
-  db.query("SELECT * from races").then(result =>{
-    res.json(result);
-  })
-});
-
-api.post('/api/query', (req, res) => {
-  const query = req.body.query;
-
-  db.query(query).then(result =>{
-    res.json(result);
-  })
-});
-
 api.listen(port)
-console.log('API is listening at localhost:'+port);
+console.log(`API is listening at localhost:${port}`);
